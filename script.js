2509074771c07847ac9ee16c6bd20802c14319f2
mariby21 <phenal21@gmail.com>
2022-12-21 09:32:04 +0200

    final comit

--- script.js
+++ script.js
@@ -1,39 +1,57 @@
 const inputFiledNextTodo = document.querySelector(".input-from");
 const btnAdd = document.querySelector(".add-btn");
 const containerDiv = document.querySelector(".container-cur");
+//To append all chields
+const wrap = document.querySelector(".wrap");
 
-// const btnDelete = document.querySelector(".btn-delete");
-// let taskValue = document.querySelector(".todo-input1");
-// const wrap = document.querySelector(".wrap");
-
+// All the events happen when addItem is shot
 function addItem() {
   todoList();
 }
-addItem();
 
 function todoList() {
   btnAdd.addEventListener("click", () => {
     if (inputFiledNextTodo.value === "") return alert("No empty field!");
 
-    containerDiv.innerHTML += ` 
-        <div class="wrap">
-         <div class="todo">
-          <input class="todo-input1" type="text" readonly value="${inputFiledNextTodo.value}" />
-        </div>
-      </div>`;
+    //Create el, btn's and atr when event is shot
+    // Todo Div
+    let todoDiv = document.createElement("div");
+    todoDiv.classList = "todo";
+    // Input filed
+    let inputFieldCurrTasks = document.createElement("input");
+    inputFieldCurrTasks.classList = "todo-input1";
+    inputFieldCurrTasks.value = inputFiledNextTodo.value;
+    // Button Check
+    let btnCheck = document.createElement("button");
+    btnCheck.classList = "btn btn-check";
+    // Check icon(need to remove pointer event)
+    let span = document.createElement("span");
+    span.classList = "material-symbols-outlined";
+    span.textContent = "check";
+    btnCheck.append(span);
+    // Delete Button
+    let btnDelete = document.createElement("button");
+    btnDelete.classList = "btn btn-delete";
+    //Trash icon
+    let spanTrash = document.createElement("span");
+    spanTrash.classList = "material-symbols-outlined";
+    spanTrash.textContent = "delete";
+    btnDelete.append(spanTrash);
+    //Apend all new childs to the parent div
+    todoDiv.append(inputFieldCurrTasks, btnCheck, btnDelete);
+    wrap.append(todoDiv);
+
+    //Buttons event's
+    btnCheck.addEventListener("click", () => {
+      inputFieldCurrTasks.classList.toggle("check");
+    });
+
+    btnDelete.addEventListener("click", () => {
+      todoDiv.remove();
+    });
 
     inputFiledNextTodo.value = "";
   });
 }
 
-function checkItem() {
-  const btnCheck = document.querySelector(".btn-check");
-  btnCheck.addEventListener("click", () => {
-    taskValue.classList.add("check");
-    console.log("load");
-  });
-}
-
-// btnDelete.addEventListener("click", () => {
-//   wrap.remove();
-// });
+addItem();
